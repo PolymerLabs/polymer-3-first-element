0a570d78dbf6bc02fcccf9543b8b0ad590b9e1a4
Kate Jeffreys <katejeffreys@google.com>
2017-12-05 20:00:49 -0800

    Remove node_modules junk_

--- node_modules/@polymer/polymer/test/unit/dynamic-imports/inner-element.js
+++
@@ -1,9 +0,0 @@
-import '../../../polymer.js';
-import { Polymer } from '../../../lib/legacy/polymer-fn.js';
-Polymer({
-  _template: `
-    <span id="content">inner-element</span>
-`,
-
-  is: 'inner-element'
-});
