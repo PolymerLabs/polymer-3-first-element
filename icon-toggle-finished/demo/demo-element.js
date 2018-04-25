c19159d1c0d9b2f5827cb40b95ee822f790e7a68
Kate Jeffreys <katejeffreys@google.com>
2018-04-25 13:11:19 -0700

    Make less horrible

--- icon-toggle-finished/demo/demo-element.js
+++ icon-toggle-finished/demo/demo-element.js
@@ -1,24 +1,9 @@
-import {Element as PolymerElement} from "../../node_modules/@polymer/polymer/polymer-element.js"
-import "../../node_modules/@polymer/iron-icons/iron-icons.js"
-import "../icon-toggle.js"
-
-//export const html = Polymer.html;
-
-export const html = (strings, ...values) => strings[0]
-+ values.map((v, i) => v + strings[i+1]).join('');
+import { PolymerElement, html } from '@polymer/polymer/polymer-element.js';
+import '@polymer/iron-icons/iron-icons.js';
+import '../icon-toggle.js';
 
 class DemoElement extends PolymerElement {
-  
-  _message(fav) {
-    if (fav) {
-      return 'You really like me!';
-    } 
-    else {
-      return 'Do you like me?';
-    }
-  }
-  
-  static get template () {
+  static get template() {
     return html`
       <style>
         :host {
@@ -30,19 +15,23 @@ class DemoElement extends PolymerElement {
       </style>
   
       <h3>Statically-configured icon-toggles</h3>
-    
       <icon-toggle toggle-icon="star"></icon-toggle>
       <icon-toggle toggle-icon="star" pressed></icon-toggle>
     
       <h3>Data-bound icon-toggle</h3>
-
       <!-- use a computed binding to generate the message -->
       <div><span>[[_message(isFav)]]</span></div>
-
       <!-- curly brackets ({{}}} allow two-way binding --> 
       <icon-toggle toggle-icon="favorite" pressed="{{isFav}}"></icon-toggle>
-    `
+    `;
+  }
+  _message(fav) {
+    if (fav) {
+      return 'You really like me!';
+    } 
+    else {
+      return 'Do you like me?';
+    }
   }
 }
-
 customElements.define('demo-element', DemoElement);
